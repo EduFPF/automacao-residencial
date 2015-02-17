1fc3baa32f41bddfe4e327a9fb7cdc5b7b612213
Eduardo <punk.lagwagon@gmail.com>
2015-02-17 10:11:43 -0300

    Arduino Funcionando

--- automaJoy.ino
+++ automaJoy.ino
@@ -1,10 +1,19 @@
-// automaJoy.ino
+// aut.ino
+
+int rele = 13;
 
 void setup() {
+
+	pinMode(rele, OUTPUT);
 
 }
 
 void loop() {
+
+	digitalWrite(rele, HIGH);
+	delay(2500);
+	digitalWrite(rele, OUTPUT);
+	delay(2500);
 
 }
 
