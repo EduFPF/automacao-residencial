c8488221d152956d12c68ed4c262acb709b16928
Eduardo <punk.lagwagon@gmail.com>
2015-02-17 19:00:15 -0300

    Cabecalho

--- automaJoy.ino
+++ automaJoy.ino
@@ -1,19 +1,34 @@
-// aut.ino
+/*
+	Eduardo Figueiredo Porto Filho
+	Universidade Federal de Campina Grande
+	Automação Residencial
+*/
 
 int rele = 13;
+char s;
 
 void setup() {
 
+	Serial.begin(9600);
 	pinMode(rele, OUTPUT);
 
 }
 
 void loop() {
 
-	digitalWrite(rele, HIGH);
-	delay(2500);
-	digitalWrite(rele, OUTPUT);
-	delay(2500);
+	if (Serial.available()) {
+		s = Serial.read();
+		Serial.println(s);
+	}
+
+
+	if (s == '1'){
+		digitalWrite(rele, LOW);
+	}
+
+	else if (s == '0'){
+		digitalWrite(rele, HIGH);
+	}
 
 }
 
